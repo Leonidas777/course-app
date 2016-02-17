33674f0df85ea3301450022350af1aa6ec9c3a15
Leo <leonidas18let@gmail.com>
2016-02-18 01:14:17 +0400

    [##113700743] Deletes the old view files and fixes invalidness of code in uploader

--- app/uploaders/base_uploader.rb
+++ app/uploaders/base_uploader.rb
@@ -3,7 +3,8 @@ class BaseUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   def store_dir
-    subfolder = Rails.env.test? ? 'test/' : '' "system/uploads/#{subfolder}#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    subfolder = Rails.env.test? ? 'test/' : ''
+    "system/uploads/#{subfolder}#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def default_url
