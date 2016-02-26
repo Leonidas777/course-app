bc1425c9f08251ac5de7e245621ada781e6a199d
Leo <leonidas18let@gmail.com>
2016-02-26 12:01:27 +0400

    [#114186777] Fixes the rb files to the haml files

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,22 +1,5 @@
-class WelcomeController < ApplicationController
-  before_action :test
-
-  def test
-    false
-  end
-
-  def wrap_with_h1
-    "<h1>#{yield}</h1>".html_safe
-  end
+class WelcomeController < ApplicationController  
 
   def index
-    @content = wrap_with_h1 do
-      %w(bruce clark peter).select do |name|
-        name != 'clark'
-      end.map(&:humanize).join(' ')
-    end
-  end
-
-  def true?
   end
 end
