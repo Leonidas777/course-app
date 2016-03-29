7605dcd670f14dc2782d4e4da2486bc086ba619e
Leo <leonidas18let@gmail.com>
2016-03-30 00:46:33 +0300

    Fixes issues after merge.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -15,33 +15,7 @@ class LessonsController < ApplicationController
   def current_course
     @current_course ||= Course.find(params[:course_id])
   end
-
-<<<<<<< HEAD
   helper_method :current_course
-=======
-  def asc_order?
-    params[:order] != 'desc'
-  end
-
-  def author?
-    current_user.id == current_course.user.id
-  end
-
-  def desc_order_lessons
-    if user_signed_in? && author?
-      return @lessons = current_course.lessons.desc_order.one_page(params[:page], params[:per_page])
-    end
-    @lessons = current_course.lessons.desc_order.one_page(params[:page], params[:per_page], true)
-  end
-
-  def asc_order_lessons
-    if user_signed_in? && author?
-      return @lessons = current_course.lessons.asc_order.one_page(params[:page], params[:per_page]) if user_signed_in? && author?
-    end
-    @lessons = current_course.lessons.asc_order.one_page(params[:page], params[:per_page], true)
-  end
-
-  helper_method :current_course, :asc_order?, :author?
 
   def load_lesson
     @lesson = Lesson.find(params[:id])
@@ -50,5 +24,4 @@ class LessonsController < ApplicationController
   def current_ability
     @current_ability ||= Ability.new(current_user, params[:shared_key])
   end
->>>>>>> lesson-4
 end
